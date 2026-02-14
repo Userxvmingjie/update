fe65ec811e130d838a6443228ac63cbe9c9fd92c
Userxvmingjie <m17390950407@outlook.com>
2026-02-14 19:17:17 +0800

    由update.download123.cc.cd上传文件 tidy.bat [ 公共上传 ]

---
+++ default/tidy.bat
@@ -0,0 +1,24 @@
+echo off
+cls
+mkdir htmls
+mkdir zips
+mkdir others
+mkdir exes
+mkdir txts
+mkdir jars
+mkdir normals
+pause
+cls
+move *.html htmls
+move *.htm htmls
+move *.zip zips
+move *.jar jars
+move *.txt txts
+move *.png normals
+move *.m4a normals
+move *.mp4 normals
+move *.crdownload others
+move *.exe exes
+move *.msi exes
+echo Done!
+pause
